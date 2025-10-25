282a512cf1559505ad9dc22a5df4520808b95a31
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-10-25 16:25:57 +0530

    Update QueueUsingArray.c

--- QueueUsingArray.c
+++ QueueUsingArray.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
-#define MAX 100
+#define MAX_SIZE 100
 
-int queue[MAX];
+int queue[MAX_SIZE];
 int front = -1, rear = -1;
 
 void enqueue(int value, int size) {
     if (rear == size - 1) {
-        printf("Queue is full\n");
+        printf("Overflow\n");
     } else {
-        if (front == -1) 
+        if (front == -1)
             front = 0;
         rear++;
         queue[rear] = value;
-        printf("Enqueued: %d\n", value);
+        printf("Enqueued value: %d\n", value);
     }
 }
 
 void dequeue() {
-    if (front == -1 || front > rear) {  
-        printf("Queue is empty\n");
+    if (front == -1 || front > rear) {
+        printf("Underflow\n");
     } else {
-        printf("Dequeued: %d\n", queue[front]);
+        printf("Dequeued value: %d\n", queue[front]);
         front++;
     }
 }
 
 void display() {
-    if (front == -1 || front > rear) { 
+    if (front == -1 || front > rear) {
         printf("Queue is empty\n");
     } else {
-        printf("Queue elements are: ");
+        printf("Queue elements: ");
         for (int i = front; i <= rear; i++) {
             printf("%d ", queue[i]);
         }
@@ -39,20 +39,21 @@ void display() {
 
 int main() {
     int choice, size, value;
-    printf("Enter the number of elements: ");
+
+    printf("Enter the queue size: ");
     scanf("%d", &size);
 
-    if (size > MAX) {
-        printf("Invalid queue size\n");
+    if (size > MAX_SIZE || size <= 0) {
+        printf("Invalid size\n");
         return -1;
     }
 
     do {
-        printf("\n------ Queue Menu ------\n");
+        printf("\n---- Queue Menu ----\n");
         printf("1. Enqueue\n");
         printf("2. Dequeue\n");
-        printf("3. Display\n");
-        printf("4. Exit\n");
+       
+        printf("3. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -67,16 +68,14 @@ int main() {
                 dequeue();
                 display();
                 break;
+            
             case 3:
-                display();
-                break;
-            case 4:
                 printf("Exiting...\n");
                 break;
             default:
-                printf("Invalid choice!\n");
+                printf("Invalid choice\n");
         }
-    } while (choice != 4);
+    } while (choice != 3);
 
     return 0;
 }
