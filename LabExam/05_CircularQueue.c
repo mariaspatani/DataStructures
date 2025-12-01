f445003e8ec14b2d81f6de5355e0e794d5bd347e
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-12-02 00:21:41 +0530

    Create 05_CircularQueue.c

---
+++ LabExam/05_CircularQueue.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+
+int q[100], rear = -1, front = -1, size;
+
+void display() {
+    if(front == -1) {
+        printf("Queue is Empty\n");
+        return;
+    }
+    int i = front;
+
+    while(1) {
+        printf("%d ", q[i]);
+        if(i == rear) break;
+        i = (i + 1) % size;
+    }
+    printf("\n");
+}
+
+void enqueue(int val) {
+    if((rear + 1) % size == front) {
+        printf("Queue full\n");
+        return;
+    }
+    if(front == -1) {
+        front = rear = 0;
+    } else {
+        rear = (rear + 1) % size;
+    }
+
+    q[rear] = val;
+    printf("%d inserted\n", val);
+    display();
+}
+
+void dequeue() {
+    if(front == -1) {
+        printf("Queue is Empty\n");
+        return;
+    }
+    printf("%d deleted\n", q[front]);
+
+    if(front == rear) {
+        front = rear = -1;
+    } else {
+        front = (front + 1) % size;
+    }
+    display();
+}
+
+int main() {
+    int choice, val;
+
+    printf("Enter queue size: ");
+    scanf("%d", &size);
+
+    while(1) {
+        printf("\n1. Enqueue  2. Dequeue  3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        if(choice == 1) {
+            printf("Enter value to insert: ");
+            scanf("%d", &val);
+            enqueue(val);
+        }
+        else if(choice == 2) {
+            dequeue();
+        }
+        else if(choice == 3) {
+            printf("Exiting...\n");
+            break;
+        }
+        else {
+            printf("Invalid choice\n");
+        }
+    }
+
+    return 0;
+}
