b33a2938a0ee7f40b79649478205c09a4c752093
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-12-01 15:18:18 +0530

    Create 11_BinarySearchWithTC&SC.c

---
+++ LabExam/11_BinarySearchWithTC&SC.c
@@ -0,0 +1,39 @@
+//Binary Search
+#include<stdio.h>
+int main(){
+int n,i,j,key;
+printf("Enter the no of elements in an sorted array: ");
+scanf("%d",&n);
+int A[10];
+printf("Enter the elements:");
+for (i=0;i<n;i++){
+    scanf("%d",&A[i]);
+}
+
+printf("\nEnter the target key value :");
+scanf("%d",&key);
+
+int found=-1;
+int low=0;
+int high=n-1;
+
+while(low<=high){
+    int mid=low+(high-low)/2;
+    if(A[mid]==key){
+        found=mid;
+        break;
+    }
+    else if(key<A[mid]){
+        high=mid-1;
+    }else{
+        low=mid+1;
+    }
+}
+if(found==-1){
+    printf("\nNo such elemnet is present ");
+}else{
+    printf("\nFound at : %d",found);
+}
+printf("\nTime Complexity: O(logn) & Space Complexity : O(1)");
+return 0;
+}
